56097a1749df935816ffc248946fb6c114b0cb67
satyam-seth <satyam1998.1998@gmail.com>
2021-12-06 01:00:25 +0530

    add Procfile

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#07f=&71-0ndw82yi(hlldl5+xq%&#*2ay=o&ffu96(jnmz%zh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
